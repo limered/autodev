c736749cb8ae94a269e62297dccaba4dc11cbdc8
limered <me@limered.de>
2026-08-29 15:42:04 +0200

    rename queue page route to /issueview

--- dashboard/src/web/src/router.js
+++ dashboard/src/web/src/router.js
@@ -6,6 +6,6 @@ export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'active', component: RunsList, meta: { title: 'active-jobs', hotkey: '1' } },
-    { path: '/queue', name: 'queue', component: DispatchView, meta: { title: 'queue', hotkey: '2' } },
+    { path: '/issueview', name: 'queue', component: DispatchView, meta: { title: 'queue', hotkey: '2' } },
   ],
 })
